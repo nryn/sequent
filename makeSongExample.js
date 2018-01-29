36b078337779577e9c2a06e33c73758c9fb8b734
nryn <narayanohanlon@gmail.com>
2018-01-29 22:25:27 +0000

    Way more detailed instructions in the example file.

--- makeSongExample.js
+++ makeSongExample.js
@@ -4,44 +4,65 @@ const Song = require('./song');
 const Phrase = require('./phrase');
 const Bar = require('./bar');
 const Note = require('./note');
+const Player = require('./player');
+const util = require('util') // tool to help us inspect objects
 
 // Let's make a song with one phrase, two bars of 4/4, each with C2s every quarter note.
+// First, make the component parts.
 
 const myFirstSong = new Song();
 const myFirstPhrase = new Phrase();
 const barOne = new Bar();
 const barTwo = new Bar();
+const trumpetC = new Note("Trumpet", "C", 2);
+
+// Now we'll put the parts together how they belong: adding the bars to the phrase, adding the phrase to the song...
 
 myFirstPhrase.addBar(barOne, barTwo);
 myFirstSong.addPhrase(myFirstPhrase);
 
-const trumpetC = new Note("Trumpet", "C", 2);
+// ...and adding the trumpet C note on the first semiquaver (sixteenth) of every beat (quarter note).
 
-barOne.beats[1][1].push(trumpetC);
-barOne.beats[2][1].push(trumpetC);
-barOne.beats[3][1].push(trumpetC);
-barOne.beats[4][1].push(trumpetC);
+barOne.beats[1][1].push(trumpetC); // The first [1] is a beat, the second can be [1],[2],[3] or [4]. That's to split each beat in to the semiquavers.
+barOne.beats[2][1].push(trumpetC); // In this example the second [number] is always 1.
+barOne.beats[3][1].push(trumpetC); // That's because we want the note to be triggered only at the very start.
+barOne.beats[4][1].push(trumpetC); // If we used all [3] on the second note, we'd have a syncopated/off-beat rhythm.
 
-// ok let's add some 2s and 4s on the second bar
+// ...and some 2s and 4s on the second bar.
 
 barTwo.beats[2][1].push(trumpetC);
 barTwo.beats[4][1].push(trumpetC);
 
-// let's add a second phrase too
+// Let's make a second phrase too, and add it to the song. we'll leave it empty in this example.
 
 const mySecondPhrase = new Phrase();
 myFirstSong.addPhrase(mySecondPhrase);
 
-// let's repeat the first phrase after (section A)
+// but now we have two phrases (section A and section B) let's repeat the first phrase.
+// this will give us an A - B - A song structure.
 
 myFirstSong.duplicatePhrase('A');
 
-// let's see what the whole song object looks like
+// Now we can see what the whole song object looks like:
+// console.log(util.inspect(myFirstSong, {showHidden: false, depth: null}))
 
-const util = require('util')
-console.log(util.inspect(myFirstSong, {showHidden: false, depth: null}))
-
-// could log JSON to stdout so can "| jq ." on command line...
+// We could log JSON to stdout to allow us to use tools like jq on command line.
 // console.log(JSON.stringify(myFirstSong));
 
-// and that's how it could be done
+// ...and that's how a song could be made!
+// ---------------------------------------
+
+// Now let's 'play' the song.
+
+// We'll first make a player.
+const myPlayer = new Player();
+
+// Then load the song into it.
+
+myPlayer.load(myFirstSong);
+
+// Now we can play it!
+
+myPlayer.play();
+
+// It's that easy.
