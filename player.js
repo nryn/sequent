6b29c01e677c3dfa6a173485e2ed99b20627d9bf
nryn <narayanohanlon@gmail.com>
2018-02-02 10:24:39 +0000

    Removing interface, since all responsibilities are really of the Player.

--- player.js
+++ player.js
@@ -61,9 +61,61 @@ Player.prototype.load = function(song) {
       };
     };
   };
+  renderGridArea(song);
 };
 
 Player.prototype.clear = function() {
   this.currentSong = {};
   this.noteBuffer = [];
+};
+
+function renderGridArea(song){
+  let playbackTable = document.getElementById('grid');
+  while (playbackTable.hasChildNodes()) {
+    playbackTable.removeChild(playbackTable.lastChild); // let's empty the grid before rendering it, just in case
+  };
+  const noteInScale = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"];
+  const instruments = song.getInstrumentList();
+
+  noteInScale.forEach(function(note) {
+    let row = document.createElement('tr');
+    row.classList.add(note);
+    row.classList.add("note_row");
+    row.classList.add("collapsed");
+    playbackTable.appendChild(row);
+    instruments.forEach(function(instrument) {
+      let noteCell = document.createElement('td');
+      noteCell.id = note + "_" + instrument;
+      noteCell.classList.add(instrument);
+      noteCell.classList.add("note_cell");
+      noteCell.classList.add("collapsed");
+      row.appendChild(noteCell);
+    });
+  });
+  expandGridArea();
+};
+
+function expandGridArea() {
+  let playbackTable = document.getElementById('grid');
+  let noteRows = document.querySelectorAll('.note_row');
+  let noteCells = document.querySelectorAll('.note_cell');
+
+  playbackTable.classList.remove("collapsed");
+
+  for ( let i=0; i < noteRows.length; i++ ) {
+    let animateRow = expandElementsFunctionFactory(i, noteRows);
+    setTimeout(animateRow, i * i * 18);
+  };
+
+  for ( let i=0; i < noteCells.length; i++ ) {
+    let animateCells = expandElementsFunctionFactory(i, noteCells);
+    setTimeout(animateCells, i * 24);
+  };
+
+  function expandElementsFunctionFactory(iterator, nodeList) {
+    var element = nodeList[iterator];
+    return function() {
+      element.classList.remove('collapsed');
+    };
+  };
 };
