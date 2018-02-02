2b3c2c6bb6431a01ac0d5f4b616d3d7dcacf9411
nryn <narayanohanlon@gmail.com>
2018-02-02 17:08:48 +0000

    Extract most of the visualiser functions out of the player.

--- playbackVisualiser.js
+++ playbackVisualiser.js
@@ -1,5 +1,43 @@
 'use strict'
 
-function Visualiser() {
+function Visualiser(player) {
+  this.player = player;
+};
 
+Visualiser.prototype.play = function() {
+  if (this.player.currentSong && this.player.noteBuffer.length > 0) {
+    let noteCounter = 0; // we'll compare this to total number of notes in the song, in order to break out of the loop at the end.
+    let semiquaverTiming = songBpmToSemiquaverMilliseconds(this.player.currentSong.tempo); // figure out smallest note timing based on the overall song tempo
+    let expectedTiming = Date.now() + semiquaverTiming; // expect a semiquaver to be as long as a semiquaver
+    setTimeout(playNote.bind(this), semiquaverTiming); // wait a semiquaver, then play a note
+    function playNote() {
+      let drift = Date.now() - expectedTiming; // check again, how long did it really take to step in to this function?
+      if (drift > semiquaverTiming) { // shouldn't happen.
+          throw "Can't reliably play back. The window might be inactive.";
+      };
+      this.renderNotesInGridCells(this.player.noteBuffer[noteCounter++]); // this key line sends the notes to be rendered in the grid
+      expectedTiming += semiquaverTiming; // add another semiquaver's worth, so we try to predict the time of our next loop
+      if (noteCounter < this.player.noteBuffer.length) {
+        setTimeout(playNote.bind(this), Math.max(0, semiquaverTiming - drift)); // settingTimeout to the same setTimeout callback we're already inside, creating a loop. takes into account drift
+      };
+    };
+  };
+};
+
+function songBpmToSemiquaverMilliseconds(bpm) {
+  return 60000 / bpm / 4; // There are 60 seconds in a minute, and 1,000 MS in a second. Then we divide by 4 to get semiquavers out of the beat.
+};
+
+Visualiser.prototype.renderNotesInGridCells = function(noteList) {
+  let tempo = this.player.currentSong.tempo;
+  noteList.forEach(function(note) {
+    let cell = document.getElementById(note.note + "_" + note.instrument);
+    let fillerDiv = document.createElement('div');
+    fillerDiv.classList.add('filler');
+    cell.appendChild(fillerDiv);
+    fillerDiv.classList.add('triggered'+note.duration);
+    setTimeout(function() {
+      cell.removeChild(fillerDiv);
+    }, 1500); // remove element after arbitrary second and a half
+  });
 };
--- player.js
+++ player.js
@@ -1,48 +1,10 @@
 'use strict'
 
 function Player() {
-  this.currentSong = getCurrentSong();
+  this.currentSong = {};
   this.noteBuffer = [];
-  this.visualiser = new Visualiser();
+  this.visualiser = new Visualiser(this);
   this.sequencer = new Sequencer();
-};
-
-Player.prototype.play = function() {
-  if (this.currentSong && this.noteBuffer.length > 0) {
-    let noteCounter = 0; // we'll compare this to total number of notes in the song, in order to break out of the loop at the end.
-    let semiquaverTiming = songBpmToSemiquaverMilliseconds(this.currentSong.tempo); // figure out smallest note timing based on the overall song tempo
-    let expectedTiming = Date.now() + semiquaverTiming; // expect a semiquaver to be as long as a semiquaver
-    setTimeout(playNote.bind(this), semiquaverTiming); // wait a semiquaver, then play a note
-    function playNote() {
-      let drift = Date.now() - expectedTiming; // check again, how long did it really take to step in to this function?
-      if (drift > semiquaverTiming) { // shouldn't happen.
-          throw "Can't reliably play back. The window might be inactive.";
-      };
-      this.renderNotesInGridCells(this.noteBuffer[noteCounter++]); // this key line sends the notes to be rendered in the grid
-      expectedTiming += semiquaverTiming; // add another semiquaver's worth, so we try to predict the time of our next loop
-      if (noteCounter < this.noteBuffer.length) {
-        setTimeout(playNote.bind(this), Math.max(0, semiquaverTiming - drift)); // settingTimeout to the same setTimeout callback we're already inside, creating a loop. takes into account drift
-      };
-    };
-  };
-};
-
-function songBpmToSemiquaverMilliseconds(bpm) {
-  return 60000 / bpm / 4; // There are 60 seconds in a minute, and 1,000 MS in a second. Then we divide by 4 to get semiquavers out of the beat.
-};
-
-Player.prototype.renderNotesInGridCells = function(noteList) {
-  let tempo = this.currentSong.tempo;
-  noteList.forEach(function(note) {
-    let cell = document.getElementById(note.note + "_" + note.instrument);
-    let fillerDiv = document.createElement('div');
-    fillerDiv.classList.add('filler');
-    cell.appendChild(fillerDiv);
-    fillerDiv.classList.add('triggered'+note.duration);
-    setTimeout(function() {
-      cell.removeChild(fillerDiv);
-    }, 1500); // remove element after arbitrary second and a half
-  });
 };
 
 Player.prototype.load = function(song) {
@@ -62,12 +24,18 @@ Player.prototype.load = function(song) {
       };
     };
   };
-  renderGridArea(song);
+  // visualiser.renderGridArea(song)
+  // sequencer.renderGridArea(song)
+  renderGridArea(song); // above instead of these
 };
 
 Player.prototype.clear = function() {
   this.currentSong = {};
   this.noteBuffer = [];
+};
+
+Player.prototype.play = function() {
+  this.visualiser.play();
 };
 
 function renderGridArea(song){
@@ -120,8 +88,3 @@ function expandGridArea() {
     };
   };
 };
-
-function getCurrentSong() {
-  let currentSong = exampleSong;
-  return currentSong || {};
-}
