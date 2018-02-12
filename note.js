77145e5ec27fdd976c3a8bc504593d2756a646f8
nryn <narayanohanlon@gmail.com>
2018-02-12 18:26:46 +0000

    now we're using audio, let's make the default note middle C

--- note.js
+++ note.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function Note(instrument = "Piano", note = "C", octave = 2, duration = 4) {
+function Note(instrument = "Piano", note = "C", octave = 4, duration = 4) {
   this.instrument = instrument;
   this.note = note;
   this.octave = octave;
