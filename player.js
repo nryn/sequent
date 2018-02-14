3ef9e50ec18528e960e8b9040904820ed219b7c8
nryn <narayanohanlon@gmail.com>
2018-02-14 19:15:52 +0000

    when loading, clear the textfield

--- player.js
+++ player.js
@@ -105,6 +105,7 @@ Player.prototype.createSong = function(givenName = "Automatic Song", givenTempo 
 
 Player.prototype.unpackEncodedSong = function() {
   let decodedSongData = atob(document.getElementById('load-sqnt-textarea').value);
+  document.getElementById('load-sqnt-textarea').value = "";
   let loadedSongData = JSON.parse(decodedSongData) || {};
   if (JSON.stringify(loadedSongData) == "{}" || !loadedSongData.name) {
     alert("sequent did not understand the song you are trying to load. sorry.");
