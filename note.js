4f26cdac06f95ea89b1ea350a1eb1fc40d19470b
nryn <narayanohanlon@gmail.com>
2018-02-14 16:14:39 +0000

    it should be more of a problem if we create a note without an instrument

--- note.js
+++ note.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function Note(instrument = "Piano", note = "C", octave = 4, duration = 4) {
+function Note(instrument = "No instrument assigned", note = "No note assigned", octave = 4, duration = 4) {
   this.instrument = instrument;
   this.note = note;
   this.octave = octave;
